4f4ae6821abdcbd46f95dd76ed7794be8c817a0d
Yirmandias <turi@insa-toulouse.fr>
2021-02-23 14:02:42 +0100

    rename main file

--- src/repl.rs
+++ src/repl.rs
@@ -9,6 +9,10 @@ use std::fmt::{Display, Formatter};
 use std::fs::File;
 use std::io::{self, Error, Read, Write};
 
+//imports for rustyline
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 const STR_HELP:&str =
 "FactBase version 0.1.0
 Commands:
@@ -271,4 +275,37 @@ impl Repl {
 
 fn help() -> ReplResult {
     Ok(ReplOk::String(STR_HELP.to_string()))
+}
+
+
+fn test_rustyline() {
+    fn main() {
+        // `()` can be used when no completer is required
+        let mut rl = Editor::<()>::new();
+        if rl.load_history("history.txt").is_err() {
+            println!("No previous history.");
+        }
+        loop {
+            let readline = rl.readline(">> ");
+            match readline {
+                Ok(line) => {
+                    rl.add_history_entry(line.as_str());
+                    println!("Line: {}", line);
+                },
+                Err(ReadlineError::Interrupted) => {
+                    println!("CTRL-C");
+                    break
+                },
+                Err(ReadlineError::Eof) => {
+                    println!("CTRL-D");
+                    break
+                },
+                Err(err) => {
+                    println!("Error: {:?}", err);
+                    break
+                }
+            }
+        }
+        rl.save_history("history.txt").unwrap();
+    }
 }

4b656c3191e4b138298f481d45b39af094f1610c
Yirmandias <turi@etud.insa-toulouse.fr>
2022-03-03 10:36:47 +0100

    add sort module

---
+++ lisp/src/modules/sort.rs
@@ -0,0 +1,19 @@
+use crate::core::structs::documentation::Documentation;
+use crate::core::structs::module::{IntoModule, Module};
+use crate::core::structs::purefonction::PureFonctionCollection;
+
+pub struct CtxSort {}
+
+impl IntoModule for CtxSort {
+    fn into_module(self) -> Module {
+        todo!()
+    }
+
+    fn documentation(&self) -> Documentation {
+        todo!()
+    }
+
+    fn pure_fonctions(&self) -> PureFonctionCollection {
+        todo!()
+    }
+}

f048dbc7d177b15999f116a37abf851f04dda49e
Yirmandias <turi@etud.insa-toulouse.fr>
2022-03-04 18:00:52 +0100

    remove comments

--- instances/gripper/actions.lisp
+++ instances/gripper/actions.lisp
@@ -1,20 +1,20 @@
 (begin
-    ;(def-action move '(?from room) '(?to room))
-    ;(def-action-model move
-    ;    '((:params (?from room) (?to room))
-    ;      (:pre-conditions (= (at-robby) ?from))
-    ;      (:effects
-    ;        (begin
-    ;            (assert 'at-robby ?to)))))
+    (def-action move '(?from room) '(?to room))
+    (def-action-model move
+        '((:params (?from room) (?to room))
+          (:pre-conditions (= (at-robby) ?from))
+          (:effects
+            (begin
+                (assert 'at-robby ?to)))))
 
-    ;(def-action pick '(?obj ball) '(?room room) '(?gripper gripper))
-    ;(def-action-model pick
-    ;    '((:params (?obj ball) (?room room) (?gripper gripper))
-    ;      (:pre-conditions (and (= (at ?obj) ?room) (= (at-robby) ?room) (null? (carry ?gripper))))
-    ;      (:effects
-    ;        (begin
-    ;            (assert `(carry ,?gripper) ?obj)
-    ;            (assert `(at ,?obj) nil)))))
+    (def-action pick '(?obj ball) '(?room room) '(?gripper gripper))
+    (def-action-model pick
+        '((:params (?obj ball) (?room room) (?gripper gripper))
+          (:pre-conditions (and (= (at ?obj) ?room) (= (at-robby) ?room) (null? (carry ?gripper))))
+          (:effects
+            (begin
+                (assert `(carry ,?gripper) ?obj)
+                (assert `(at ,?obj) nil)))))
 
     (def-action drop '(?obj ball) '(?room room) '(?gripper gripper))
     (def-action-model drop

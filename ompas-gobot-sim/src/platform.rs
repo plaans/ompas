f6086120f7a0802137b391e5aecd43d7a7de7db0
Yirmandias <turi@insa-toulouse.fr>
2023-06-14 16:35:37 +0200

    set timeout to init gobot-sim project

--- ompas-gobot-sim/src/platform.rs
+++ ompas-gobot-sim/src/platform.rs
@@ -311,7 +311,7 @@ impl PlatformGobotSim {
         if !sh.path_exists(format!("{}/.import", sh.current_dir().to_str().unwrap())) {
             print!("Init gobot-sim project in godot...");
             let gd_path = self.godot3_path().unwrap();
-            cmd!(sh, "{gd_path} -e").quiet().run().unwrap();
+            let _ = cmd!(sh, "timeout 7 {gd_path} -e").quiet().run();
             println!("Ok!");
         }
     }

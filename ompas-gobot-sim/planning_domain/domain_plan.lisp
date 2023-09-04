d4c3b63f328f20ed7c139fb87774e97dd4996e55
Yirmandias <turi@insa-toulouse.fr>
2023-09-04 11:11:38 +0200

    stable

--- ompas-gobot-sim/planning_domain/domain_plan.lisp
+++ ompas-gobot-sim/planning_domain/domain_plan.lisp
@@ -3,8 +3,8 @@
     (read planning_domain/base.lisp)
     (read planning_domain/lambdas.lisp)
     (read planning_domain/plan_model.lisp)
-    (read planning_domain/problems/j06.lisp)
-    ;(read planning_domain/problems/j01.lisp)
+    ;(read planning_domain/problems/j06.lisp)
+    (read planning_domain/problems/j01.lisp)
 
     (start-with-planner false)
     ;(start)
